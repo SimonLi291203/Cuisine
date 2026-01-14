7827f15fbdaaf7e765b8b0634ddfd9e1282c59f5
SimonLi291203 <lizeesimon1203@gmail.com>
2026-01-14 23:06:45 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,19 @@
-// Configuration Firebase
-const firebaseConfig = {
-  apiKey: "TON_API_KEY",
-  authDomain: "TON_PROJET.firebaseapp.com",
-  projectId: "TON_PROJET",
-  storageBucket: "TON_PROJET.appspot.com",
-  messagingSenderId: "TON_ID",
-  appId: "TON_APP_ID"
-};
+<!-- Firebase -->
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore-compat.js"></script>
+<script>
+  // Ta config Firebase copiée ici, légèrement adaptée
+  const firebaseConfig = {
+    apiKey: "UeYQT2LmmLrbuix2-AFJZT47dd2E",
+    authDomain: "recettes-personnelles.firebaseapp.com",
+    projectId: "recettes-personnelles",
+    storageBucket: "recettes-personnelles.appspot.com",
+    messagingSenderId: "3230470",
+    appId: "1:323047086323:web:eb7a6a59a0cce03f86",
+    measurementId: "G-C16RTE39L8"
+  };
 
-// Initialisation Firebase
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+  // Initialisation Firebase
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
+</script>
